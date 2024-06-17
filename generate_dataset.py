5fcdcac24c94c5e809f353a231c4547625012399
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-06-17 12:25:38 -0500

    generate_dataset: generate_videos() function for video datasets
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- generate_dataset.py
+++ generate_dataset.py
@@ -80,6 +80,45 @@ def generate_images(dataset_type, n=100000, frame_size=(64, 64), patch_size=18,
     plt.xlabel("Number of objects")
     plt.savefig('gen_{}_distribution.png'.format(get_date_str()))
 
+def generate_videos(dataset_type, n=100000, frame_size=(64, 64), patch_size=18,
+                    timesteps=10, delta_t=0.3, count_distrib={0: 1/3, 1: 1/3,
+                    2: 1/3}, allow_overlap=True):
+    # Generate sprites and labels
+    print("generating sprites...")
+    if dataset_type == 'dsprites':
+        sprites, labels = generate_dsprites(patch_size)
+    elif dataset_type == 'binary_mnist':
+        sprites, labels = generate_binary_mnist(patch_size)
+    else:
+        raise NotImplementedError
+
+    # Show sprites
+    show_img_grid(8, sprites, random_selection=True,
+                  fname='gen_{}_sprites.png'.format(get_date_str()))
+
+    # Create dataset
+    print("generating dataset...")
+    ch = sprites[0].shape[-1]
+    img_shape = (*frame_size, ch)
+    dataset, n_obj, labels = generate_video_dataset(n, img_shape, sprites,
+                                                    labels,
+                                                    count_distrib=count_distrib,
+                                                    allow_overlap=allow_overlap)
+    print("done")
+    print("shape:", dataset.shape)
+
+    # Number of objects is part of the labels
+    labels['n_obj'] = n_obj
+
+    # Save dataset
+    print("saving...")
+    root = os.path.join('generated', dataset_type)
+    os.makedirs(root, exist_ok=True)
+    file_str = get_date_str()
+    fname = 'multi_' + dataset_type + '_videos_' + file_str
+    fname = os.path.join(root, fname)
+    np.savez_compressed(fname, x=dataset, labels=labels)
+    print('done')
 
 def parse_args():
     parser = argparse.ArgumentParser(
