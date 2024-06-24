da809473692e8b5f9016d4b4e4b82a20926bbf1d
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-06-24 15:17:15 -0500

    multiobject.pytorch.SpritesVideo: punch_frame() method performing punching
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- multiobject/pytorch.py
+++ multiobject/pytorch.py
@@ -10,6 +10,7 @@ from torch.utils.data._utils.collate import default_collate
 
 class SpritesVideo(torch.nn.Module):
     DISTANCE_TO_SCREEN = 106
+    PUNCH_OUT_COLOR = 1
     SCREEN_DIMS = (100, 62)
     SCREEN_HALFWIDTH_DEGREES = np.degrees(np.arctan(
         SCREEN_DIMS[0] / 2 / DISTANCE_TO_SCREEN
@@ -55,6 +56,26 @@ class SpritesVideo(torch.nn.Module):
     @property
     def num_sprites(self):
         return self.xs.shape[0]
+
+    def punch_frame(frame, punchout=True):
+        frame = frame.numpy()
+
+        for (x, y, rx, ry, theta) in self.rfs:
+            x, y = SpritesVideo.coords_to_pixels(x, y)
+            x = round(SpritesVideo.SCREEN_RES[0] / 2 + x)
+            y = round(SpritesVideo.SCREEN_RES[1] / 2 - y)
+            rx, ry = SpritesVideo.coords_to_pixels(rx, ry)
+            rx, ry = round(rx), round(ry)
+            c = SpritesVideo.PUNCH_OUT_COLOR
+
+            mask = np.zeros(frame.shape, np.uint8)
+            mask = cv.ellipse(mask, (x, y), (rx, ry), theta, 0, 360, (c, c, c),
+                              -1)
+            if punchout:
+                frame = np.where(mask > 0, mask, frame)
+            else:
+                frame = np.where(mask > 0, frame, mask + c)
+            return frame
 
     def render(self):
         video = []
